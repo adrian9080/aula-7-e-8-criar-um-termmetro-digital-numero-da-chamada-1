3df4aafe0bf8b9b056b9f560fcbe4c7de308c753
adrian9080 <00001129489929sp@al.educacao.sp.gov.br>
2025-03-01 19:56:03 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Temperatura")
+})
+basic.showString("Temperatura")
+basic.forever(function () {
+    basic.showArrow(ArrowNames.West)
+})
